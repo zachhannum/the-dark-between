2172b7856716721d6158f442d1605ed2849c9529
Zach Hannum <zacharyhannum@gmail.com>
2022-06-29 10:49:27 -0400

    Add prototype for map viewer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,40 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import styled from "styled-components";
+import AljieudumMap from "../public/images/aljieudum.jpg";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+
+const ImageContainer = styled.div`
+  width: 30vw;
+  position: relative;
+`;
+
+const Loading = styled.div`
+  height: 100%;
+  width: 100%;
+  background-color: gray;
+  position: absolute;
+  top: 0;
+  z-index: -1;
+`;
 
 const Home: NextPage = () => {
-  return <div>Hello world!</div>;
+  return (
+    <TransformWrapper>
+      <TransformComponent>
+        <ImageContainer>
+          <Image
+            src={AljieudumMap}
+            alt="map of aljieudum"
+            layout="responsive"
+            unoptimized={true}
+          />
+          <Loading />
+        </ImageContainer>
+      </TransformComponent>
+    </TransformWrapper>
+  );
 };
 
 export default Home;
